41b025647ec6f7add354e5b84629c60f3c507979
Winzlieb <winzlieb@c3d2.de>
2026-05-19 18:51:02 +0200

    Default static URL to include /static path prefix

--- components/config.ts
+++ components/config.ts
@@ -1,6 +1,6 @@
 const config =  {
   apiURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/graphql',
-  staticContentURL: process.env.NEXT_PUBLIC_STATIC_URL || 'http://localhost:3001'
+  staticContentURL: process.env.NEXT_PUBLIC_STATIC_URL || 'http://localhost:3001/static'
 }
 
 export default config
